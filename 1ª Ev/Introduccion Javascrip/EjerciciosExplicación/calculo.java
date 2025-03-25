dc41f79692c32df6829bce1a0b0495b52435ff21
CojoCosta <dcosta1882000@gmail.com>
2025-03-25 09:48:06 +0100

    changes

--- 1ª Ev/Introduccion Javascrip/EjerciciosExplicación/calculo.java
+++
@@ -1,8 +0,0 @@
-public class calculo{
-    public static void main(String[] args) {
-        double suma = 39458229;
-        System.out.println(suma);
-        double calculo1 = suma%23;
-        System.out.println(calculo1);
-    }
-}
